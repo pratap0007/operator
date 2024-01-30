97c7e7a4472ca2859879b2ccf5b80703c7375f32
pratap0007 <shverma@redhat.com>
2024-01-30 19:40:17 +0530

    adds validations and default setting logic, and also adds tests

--- pkg/reconciler/openshift/openshiftpipelinesascode/transform_test.go
+++ pkg/reconciler/openshift/openshiftpipelinesascode/transform_test.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	mf "github.com/manifestival/manifestival"
+	routev1 "github.com/openshift/api/route/v1"
 	"github.com/tektoncd/operator/pkg/apis/operator/v1alpha1"
 	"github.com/tektoncd/pipeline/test/diff"
 	"gotest.tools/v3/assert"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -89,8 +91,30 @@ func TestUpdateAdditionControllerRoute(t *testing.T) {
 		SecretName:    "test-secret",
 	}
 	updatedManifest, err := manifest.Transform(updateAdditionControllerRoute(&additionalPACConfig, "test"))
+	if err != nil {
+		assert.NilError(t, err)
+	}
+
+	route := &routev1.Route{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(updatedManifest.Resources()[0].Object, route)
+	if err != nil {
+		assert.NilError(t, err)
+	}
+
+	expectedData := path.Join("testdata", "test-expected-additional-pac-route.yaml")
+	expectedManifest, err := mf.ManifestFrom(mf.Recursive(expectedData))
 	assert.NilError(t, err)
-	assert.DeepEqual(t, updatedManifest.Resources()[0].GetName(), "test-controller")
+
+	expectedRoute := &routev1.Route{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(expectedManifest.Resources()[0].Object, expectedRoute)
+	if err != nil {
+		assert.NilError(t, err)
+	}
+
+	if d := cmp.Diff(route, expectedRoute); d != "" {
+		t.Errorf("failed to update additional pac controller route %s", diff.PrintWantGot(d))
+	}
+
 }
 
 func TestUpdateAdditionControllerServiceMonitor(t *testing.T) {
@@ -121,4 +145,41 @@ func TestUpdateAdditionControllerConfigMapWithDefaultCM(t *testing.T) {
 	updatedManifest, err := manifest.Transform(updateAdditionControllerConfigMap(&additionalPACConfig, "test"))
 	assert.NilError(t, err)
 	assert.DeepEqual(t, updatedManifest.Resources()[0].GetName(), "pipelines-as-code")
+}
+
+func TestUpdateAdditionControllerConfigMap(t *testing.T) {
+	testData := path.Join("testdata", "test-additional-pac-cm.yaml")
+	manifest, err := mf.ManifestFrom(mf.Recursive(testData))
+	assert.NilError(t, err)
+
+	additionalPACConfig := v1alpha1.AdditionalPACControllerConfig{
+		ConfigMapName: "test-config",
+		SecretName:    "test-secret",
+		Settings:      map[string]string{"application-name": "Test CI application", "hub-url": "https://custom-hub.com"},
+	}
+
+	updatedManifest, err := manifest.Transform(updateAdditionControllerConfigMap(&additionalPACConfig, "test"))
+	if err != nil {
+		assert.NilError(t, err)
+	}
+
+	cm := &corev1.ConfigMap{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(updatedManifest.Resources()[0].Object, cm)
+	if err != nil {
+		assert.NilError(t, err)
+	}
+
+	expectedTestData := path.Join("testdata", "test-expected-additional-pac-cm.yaml")
+	expectedManifest, err := mf.ManifestFrom(mf.Recursive(expectedTestData))
+	assert.NilError(t, err)
+	expectedCM := &corev1.ConfigMap{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(expectedManifest.Resources()[0].Object, expectedCM)
+	if err != nil {
+		assert.NilError(t, err)
+	}
+	assert.NilError(t, err)
+
+	if d := cmp.Diff(cm, expectedCM); d != "" {
+		t.Errorf("failed to update additional pac controller route %s", diff.PrintWantGot(d))
+	}
 }
