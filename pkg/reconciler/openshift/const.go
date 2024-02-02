307b2b3ff94913c89f4014fca238aa22817791e9
Piyush Garg <pgarg@redhat.com>
2024-02-02 15:36:28 +0530

    Refactor reconciler

--- pkg/reconciler/openshift/const.go
+++ pkg/reconciler/openshift/const.go
@@ -4,6 +4,5 @@ const (
 	OperandOpenShiftPipelinesAddons = "openshift-pipelines-addons"
 	OperandOpenShiftPipelineAsCode  = "openshift-pipeline-as-code"
 	// NamespaceSCCAnnotation is used to set SCC for a given namespace
-	NamespaceSCCAnnotation                             = "operator.tekton.dev/scc"
-	OperandOpenShiftPipelineAsCodeAdditionalController = "openshift-pipeline-as-code-additional-controller"
+	NamespaceSCCAnnotation = "operator.tekton.dev/scc"
 )
